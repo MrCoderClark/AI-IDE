1ac16b712b8c1da1b88f74f8e84a2c0033904f5b
Joseph Clark <weirdlookingjay@live.com>
2026-02-05 14:33:42 -0500

    04: create-background-jobs

---
+++ src/app/api/demo/blocking/route.ts
@@ -0,0 +1,14 @@
+// import { google } from "@ai-sdk/google";
+import { anthropic } from "@ai-sdk/anthropic";
+import { generateText } from "ai";
+import { NextResponse } from "next/server";
+
+export async function POST() {
+  const response = await generateText({
+    // model: google("gemini-2.5-flash"),
+    model: anthropic("claude-3-haiku-20240307"),
+    prompt: "Write a vegetarian lasagna recipe for 4 people",
+  });
+
+  return NextResponse.json({ response });
+}
