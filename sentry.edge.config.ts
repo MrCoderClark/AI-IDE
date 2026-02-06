7e96ac344f7462b8cba3f4f0b3c048c7bf09f6f6
Joseph Clark <weirdlookingjay@live.com>
2026-02-06 11:42:02 -0500

    06: add-error-tracking

---
+++ sentry.edge.config.ts
@@ -0,0 +1,24 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://1c2b9bdaf42057417ba1385fd43dff95@o4510839786766336.ingest.us.sentry.io/4510839794434048",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+  integrations: [
+    Sentry.vercelAIIntegration(),
+    Sentry.consoleIntegration({ levels: ["log", "warn", "error"] }),
+  ],
+});
--- src/app/demo/page.tsx
+++ src/app/demo/page.tsx
@@ -3,7 +3,12 @@
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
 
+import * as Sentry from "@sentry/nextjs"
+import { useAuth } from "@clerk/nextjs";
+
 export default function DemoPage() {
+  const { userId } = useAuth();
+
   const [loading, setLoading] = useState(false);
   const [loading2, setLoading2] = useState(false);
 
@@ -19,6 +24,22 @@ export default function DemoPage() {
     setLoading(false);
   };
 
+  // 1) Client error - throws in the browser
+  const handleClientError = () => {
+    Sentry.logger.info("User attempting to click on client function", { userId });
+    throw new Error("Client error: Something went wrong in the browser!")
+  }
+  // 2) API error - triggers server-side error
+  const handleCApiError = async () => {
+    await fetch("/api/demo/error", { method: "POST" })
+  }
+
+  // 3) Inngest error - triggers error in background job
+  const handleInngestError = async () => {
+    await fetch("/api/demo/inngest-error", { method: "POST" })
+  }
+
+
   return (
     <div className="p-8 space-x-4">
       <Button onClick={handleBlocking}>
@@ -27,6 +48,18 @@ export default function DemoPage() {
 
       <Button onClick={handleBackground}>
         {loading2 ? "Loading..." : "Background"}
+      </Button>
+
+      <Button variant={"destructive"} onClick={handleClientError}>
+        Client Error
+      </Button>
+
+      <Button variant={"destructive"} onClick={handleCApiError}>
+        API Error
+      </Button>
+
+      <Button variant={"destructive"} onClick={handleInngestError}>
+        Inngest Error
       </Button>
     </div>
   );
