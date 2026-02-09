f1830ee4c6fc4f7596ccc5791fed6f075bcfe6c9
Joseph Clark <weirdlookingjay@live.com>
2026-02-09 13:54:27 -0500

    08: create-ide-layout

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -7,7 +7,7 @@ import {
   ConvexReactClient,
   Unauthenticated,
 } from "convex/react";
-import { ClerkProvider, useAuth, UserButton } from "@clerk/nextjs";
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { ThemeProvider } from "./theme-provider";
 import { UnauthenticatedView } from "@/features/auth/components/unauthenticated-view";
@@ -27,7 +27,6 @@ export const Providers = ({ children }: { children: ReactNode }) => {
           disableTransitionOnChange
         >
           <Authenticated>
-            <UserButton />
             {children}
           </Authenticated>
           <Unauthenticated>
