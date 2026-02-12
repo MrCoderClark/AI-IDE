efc8002d460ea5ddd4ceffeefac996c3858568db
Joseph Clark <weirdlookingjay@live.com>
2026-02-12 11:21:21 -0500

    09: add-file-explorer

---
+++ src/features/projects/components/file-explorer/constants.ts
@@ -0,0 +1,10 @@
+// BASE padding for root level items (after project header)
+export const BASE_PADDING = 12;
+// Additional padding per nexting level
+export const LEVEL_PADDING = 12;
+
+export const getItemPadding = (level: number, isFile: boolean) => {
+  // Files need extra padding since they don't have the chevron
+  const fileOffset = isFile ? 16 : 0;
+  return BASE_PADDING + level * LEVEL_PADDING + fileOffset;
+};
