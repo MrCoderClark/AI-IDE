2fe98a529fd9417d7fe06fe2c5c7572d183b036f
Joseph Clark <weirdlookingjay@live.com>
2026-02-17 15:41:41 -0500

    10: add-code-editor-and-state

--- src/features/projects/components/file-explorer/tree.tsx
+++ src/features/projects/components/file-explorer/tree.tsx
@@ -9,6 +9,7 @@ import { LoadingRow } from "./loading-row"
 import { getItemPadding } from "./constants"
 import { CreateInput } from "./create-input"
 import { RenameInput } from "./rename-input"
+import { useEditor } from "@/features/editor/hooks/use-editor"
 
 
 export const Tree = ({
@@ -28,6 +29,8 @@ export const Tree = ({
     const deleteFile = useDeleteFile()
     const createFile = useCreateFile()
     const createFolder = useCreateFolder()
+
+    const { openFile, closeTab, activeTabId } = useEditor(projectId)
 
     const folderContents = useFolderContents({
         projectId,
@@ -71,6 +74,7 @@ export const Tree = ({
 
     if (item.type === "file") {
         const fileName = item.name
+        const isActive = activeTabId === item._id
 
         if (isRenaming) {
             return (
@@ -88,12 +92,13 @@ export const Tree = ({
             <TreeItemWrapper
                 item={item}
                 level={level}
-                isActive={false}
-                onClick={() => { }}
-                onDoubleClick={() => { }}
+                isActive={isActive}
+                onClick={() => openFile(item._id, { pinned: false })}
+                onDoubleClick={() => openFile(item._id, { pinned: true })}
                 onRename={() => setIsRenaming(true)}
                 onDelete={async () => {
                     try {
+                        closeTab(item._id)
                         await deleteFile({ id: item._id })
                     } catch (e) {
                         console.error(e)
